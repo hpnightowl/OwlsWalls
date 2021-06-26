4d88d2894a1a9636f0a17a8aff7560e763397dbd
hpnightowl <iamhp2k@gmail.com>
2021-06-27 01:53:11 +0530

    owlswalls: card style showcase

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,7 +10,8 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
           body: Container(
             padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
-            child: Row(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                     "Wallpapers",
@@ -19,6 +21,44 @@ class HomePage extends StatelessWidget {
                     fontWeight: FontWeight.w500
                   ),
                 ),
+                SizedBox(height: 20,),
+                Row(
+                 children: [
+                   Expanded(
+                       child: Column(
+                         children: [
+                           AspectRatio(
+                             aspectRatio:2/3,
+                             child:
+                             Container(
+                               decoration: BoxDecoration(
+                                   color: Colors.black,
+                                   borderRadius: BorderRadius.circular(8)
+                               ),
+                             ),
+                           )
+                         ],
+                       )
+                   ),
+                   SizedBox(width: 10,),
+                   Expanded(
+                       child: Column(
+                         children: [
+                           AspectRatio(
+                             aspectRatio: 0.8,
+                             child:
+                             Container(
+                               decoration: BoxDecoration(
+                                   color: Colors.black,
+                                   borderRadius: BorderRadius.circular(8)
+                               ),
+                             ),
+                           )
+                         ],
+                       )
+                   ),
+                 ],
+                )
               ],
             ),
           ),
